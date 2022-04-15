cb9532749bf3278de00456326e3db2b2724b1499
John <john.farah@icloud.com>
2022-04-15 08:02:04 +0100

    Update Slider.js and testimonial sections

--- js/slider.js
+++ js/slider.js
@@ -1,28 +1,11 @@
 (function () {
-    var slider = function () {
+    const slider = function () {
 
-        var carouselRoom = document.querySelectorAll('.carousel-room');
-
-        if (carouselRoom.length > 0) {
-            var carouselRoom = tns({
-                container: '.carousel-room',
-                items: 1,
-                mode: 'carousel',
-                autoplay: true,
-                animateIn: 'tns-fadeIn',
-                animateOut: 'tns-fadeOut',
-                speed: 700,
-                nav: true,
-                controls: false,
-                autoplayButtonOutput: false,
-            });
-        }
-
-        var carouselSlider = document.querySelectorAll('.carousel-testimony');
+        const carouselSlider = document.querySelectorAll('.carousel-testimony');
 
         if (carouselSlider.length > 0) {
 
-            var testimonySlider = tns({
+             testimonySlider = tns({
                 container: '.carousel-testimony',
                 items: 1,
                 mode: 'carousel',
